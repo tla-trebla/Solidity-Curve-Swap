810f33bccea7ff434343febb1482164583c92918
Albert Pangestu <stamina.left@gmail.com>
2025-03-24 23:19:45 +0700

    Moved `MockToken` into separate file at the `test` target

--- test/SwapContractTests.t.sol
+++ test/SwapContractTests.t.sol
@@ -3,51 +3,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {AbstractToken} from "../src/AbstractToken.sol";
 import {SwapContract} from "../src/SwapContract.sol";
-
-contract MockToken is AbstractToken {
-    string public name;
-    string public symbol;
-    mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowances;
-    bool private transferShouldFail = false;
-
-    constructor(string memory _name, string memory _symbol) {
-        name = _name;
-        symbol = _symbol;
-    }
-
-    function mint(address to, uint256 amount) public override {
-        balances[to] += amount;
-    }
-
-    function approve(address spender, uint256 amount) public override {
-        allowances[msg.sender][spender] = amount;
-    }
-
-    function allowance(address owner, address spender) public view override returns (uint256) {
-        return allowances[owner][spender];
-    }
-
-    function balanceOf(address account) public view override returns (uint256) {
-        return balances[account];
-    }
-
-    function setTransferShouldFail(bool shouldFail) public {
-        transferShouldFail = shouldFail;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        require(balances[from] >= amount, "Insufficient balance");
-        require(allowances[from][msg.sender] >= amount, "Allowance exceeded");
-        require(!transferShouldFail, "MockToken: transferFrom failed");
-
-        balances[from] -= amount;
-        balances[to] += amount;
-        allowances[from][msg.sender] -= amount;
-
-        return true;
-    }
-}
+import {MockToken} from "./MockToken.sol";
 
 contract SwapContractTest is Test {
     function test_Initialization_ShouldStartWithZeroBalances() public {
