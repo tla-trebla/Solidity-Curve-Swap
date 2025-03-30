89be6d35c5a0d7874de6cb3b4e4ec9796f2878ab
Albert Pangestu <stamina.left@gmail.com>
2025-03-30 23:39:51 +0700

    Added test case, when it has imbalances reserves, it should not gave any invariant

--- test/InvariantCalculatorTests.t.sol
+++ test/InvariantCalculatorTests.t.sol
@@ -81,4 +81,17 @@ contract InvariantCalculatorTest is Test {
         uint256 expectedInvariant = 5999925937812179024969;
         assertEq(invariant, expectedInvariant, "Invariant calculation is incorrect");
     }
+
+    function test_ImbalancedReserves_ShouldRevertOnNonConvergence() public {
+        uint256 expectedTokenCount = 3;
+        InvariantCalculator calculator = new InvariantCalculator(expectedTokenCount);
+
+        uint256[] memory imbalancedReserves = new uint256[](expectedTokenCount);
+        imbalancedReserves[0] = 1e18;
+        imbalancedReserves[1] = 1;    
+        imbalancedReserves[2] = 1;    
+
+        vm.expectRevert("Invariant calculation did not converge");
+        calculator.getInvariant(imbalancedReserves);
+    }
 }
